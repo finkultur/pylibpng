99d6224305df24bc1fcaebd232cecce86308dba3
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-04-02 03:13:26 +0200

    iv: add zoom option

--- iv.py
+++ iv.py
@@ -3,11 +3,10 @@
 import pygame, sys
 import pylibpng
 
-def show_png(surface, png):
+def show_png(surface, png, zoom=1):
     for h in range(0, png.height):
         for w in range(0, png.width):
-            surface.fill(png.pixels[h][w], ((w,h), (1,1)))
-
+            surface.fill(png.pixels[h][w], ((w*zoom,h*zoom), (zoom,zoom)))
 
 if __name__ == '__main__':
 
@@ -17,7 +16,7 @@ if __name__ == '__main__':
     screen.fill(pygame.Color(255,255,255))
 
     pic = pylibpng.PNG(sys.argv[1])
-    show_png(screen, pic)
+    show_png(screen, pic, 4)
 
     while True:
         for event in pygame.event.get():
