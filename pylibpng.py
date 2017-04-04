3055842954a6dd48ab84dc6a87d020c209b166be
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-04-04 20:40:29 +0200

    Some refactoring

--- pylibpng.py
+++ pylibpng.py
@@ -3,7 +3,6 @@
 import sys
 import struct
 import datetime
-import binascii
 import zlib
 import math
 
@@ -14,6 +13,10 @@ def unpack(s):
     if len(s) == 4: return struct.unpack('!I', s)[0]
     if len(s) == 8: return struct.unpack('!Q', s)[0]
     else: return -1
+
+def get_until_null(s, start=0):
+    end = s.find('\0')
+    return end+1, s[start:end]
 
 class PNG(object):
     def __init__(self, filename):
@@ -41,7 +44,7 @@ class PNG(object):
     @staticmethod
     def check_crc(chunk_id, chunk_data, crc):
         """ Check that crc(chunk_id+chunk_data) == crc """
-        calc_crc = binascii.crc32(chunk_id + chunk_data) & 0xffffffff
+        calc_crc = zlib.crc32(chunk_id + chunk_data) & 0xffffffff
         if (crc != calc_crc):
             print("CRC mismatch, file corrupted")
             return
@@ -73,6 +76,8 @@ class PNG(object):
             self.get_idat(chunk_data, next_chunk_id != 'IDAT')
             f.seek(f.tell()-8)
         elif chunk_id == "IEND":
+            defiltered_data = self.defilter(self.idat, self.width, self.height, self.pixel_size)
+            self.pixels = PNG.init_pixels(defiltered_data, self.width, self.height, self.pixel_size)
             return
 
         # Process next chunk
@@ -87,6 +92,11 @@ class PNG(object):
         self.comp_method = unpack(data[10])
         self.filter_method = unpack(data[11])
         self.interlace_method = unpack(data[12])
+
+        if self.color_type == 2:
+            self.pixel_size = 3
+        elif self.color_type == 6:
+            self.pixel_size = 4
         print("""width: %i, height: %i, bit_depth: %i, color_type: %i, comp_method: %i,
                  filter_method: %i, interlace_method: %i""" %
               (self.width, self.height, self.bit_depth, self.color_type, self.comp_method,
@@ -94,15 +104,13 @@ class PNG(object):
 
     def get_time(self, data):
         """ tIME Chunk. Timestamp in UTC """
-        year, month, day, hour, minute, second = struct.unpack('!hBBBBB', data)
-        self.timestamp = datetime.datetime(year, month, day, hour, minute, second)
+        if len(data) == 7:
+            year, month, day, hour, minute, second = struct.unpack('!hBBBBB', data)
+            self.timestamp = datetime.datetime(year, month, day, hour, minute, second)
 
     def get_itxt(self, data):
         """ iTXt Chunk """
         # TODO: Uncompress compressed data
-        def get_until_null(s, start=0):
-            end = s.find('\0')
-            return end+1, s[start:end]
         pos, self.kw = get_until_null(data)
         comp_flag = unpack(data[pos]); pos += 1
         comp_method = unpack(data[pos]); pos += 1
@@ -144,11 +152,10 @@ class PNG(object):
 
     def get_phys(self, data):
         """ pHYs Chunk """
-        pass # Not that important
+        pass
 
     def get_idat(self, data, last):
         """ IDAT Chunk """
-        # TODO: Check check_value
         # TODO: Check that data is at least "a few" bytes
         start = 0
         end = len(data)
@@ -165,56 +172,61 @@ class PNG(object):
             self.idat += self.dc_obj.flush()
             if (zlib.adler32(self.idat) & 0xffffffff) != idat_adler:
                 print("IDAT Adler calculation mismatch")
-            decomp_bytes = list(struct.unpack('!' + 'B'*len(self.idat), self.idat))
-            self.pixels = self.defilter(decomp_bytes)
+            self.idat = list(struct.unpack('!' + 'B'*len(self.idat), self.idat))
 
-    def defilter(self, data):
+    @staticmethod
+    def defilter(data, width, height, pixel_size):
         """ Defilter image data """
-        if self.color_type == 2: pixel_size = 3
-        elif self.color_type == 6: pixel_size = 4
-        row_size = self.width * pixel_size + 1
+        row_size = width * pixel_size + 1
         a = [data[i:i+row_size] for i in range(0, len(data), row_size)]
+        a[0] = PNG.defilter_scanline(a[0], [0]*row_size, pixel_size)
+        for y in range(1, height):
+            a[y] = PNG.defilter_scanline(a[y], a[y-1], pixel_size)
+        return a
 
-        for y in range(0, self.height):
-            filter_type = a[y][0]
-            a[y][0] = 0
-            # if filter_type == 0: # NONE
-            if filter_type == 1: # SUB
-                for x in range(pixel_size, row_size):
-                    a[y][x] = (a[y][x] + a[y][x-pixel_size]) % 256
-            elif filter_type == 2: # UP
-                for x in range(1, row_size):
-                    a[y][x] = (a[y][x] + a[y-1][x]) % 256
-            elif filter_type == 3: # AVERAGE
-                for x in range(1, row_size):
-                    recon_a = a[y][x-pixel_size]
-                    recon_b = a[y-1][x] if y > 1 else 0
-                    a[y][x] = (a[y][x] + math.floor((recon_a + recon_b) / 2)) % 256
-            elif filter_type == 4: # PAETH
-                def paeth_predictor(a, b, c):
-                    p = a + b - c
-                    pa = abs(p - a)
-                    pb = abs(p - b)
-                    pc = abs(p - c)
-                    if pa <= pb and pa <= pc:
-                        pr = a
-                    elif pb <= pc:
-                        pr = b
-                    else:
-                         pr = c
-                    return pr
-                for x in range(1, row_size):
-                    recon_a = a[y][x-pixel_size] if x > pixel_size else 0
-                    recon_b = a[y-1][x] if y > 1 else 0
-                    recon_c = a[y-1][x-pixel_size] if x > pixel_size else 0
-                    a[y][x] = (a[y][x] + paeth_predictor(recon_a, recon_b, recon_c)) % 256
+    @staticmethod
+    def defilter_scanline(scanline, prev, pixel_size):
+        """ Defilter a single scanline """
+        filter_type = scanline[0]
 
-        return self.init_pixels(a, pixel_size)
+        if filter_type == 0: # NONE
+            return scanline
+        elif filter_type == 1: # SUB
+            for x in range(pixel_size, len(scanline)):
+                scanline[x] = (scanline[x] + scanline[x-pixel_size]) % 256
+        elif filter_type == 2: # UP
+            for x in range(1, len(scanline)):
+                scanline[x] = (scanline[x] + prev[x]) % 256
+        elif filter_type == 3: # AVERAGE
+            for x in range(1, len(scanline)):
+                recon_a = scanline[x-pixel_size] if x > pixel_size else 0
+                recon_b = prev[x]
+                scanline[x] = (scanline[x] + math.floor((recon_a + recon_b) / 2)) % 256
+        elif filter_type == 4: # PAETH
+            def paeth_predictor(a, b, c):
+                p = a + b - c
+                pa = abs(p - a)
+                pb = abs(p - b)
+                pc = abs(p - c)
+                if pa <= pb and pa <= pc:
+                    pr = a
+                elif pb <= pc:
+                    pr = b
+                else:
+                    pr = c
+                return pr
+            for x in range(1, len(scanline)):
+                recon_a = scanline[x-pixel_size] if x > pixel_size else 0
+                recon_b = prev[x]
+                recon_c = prev[x-pixel_size] if x > pixel_size else 0
+                scanline[x] = (scanline[x] + paeth_predictor(recon_a, recon_b, recon_c)) % 256
+        return scanline
 
-    def init_pixels(self, a, pixel_size):
+    @staticmethod
+    def init_pixels(a, width, height, pixel_size):
         pixels = []
-        row_size = self.width * pixel_size + 1
-        for y in range(0, self.height):
+        row_size = width * pixel_size + 1
+        for y in range(0, height):
             scanline = []
             for x in xrange(1, row_size, pixel_size):
                 if pixel_size == 3:
