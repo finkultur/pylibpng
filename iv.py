d7f8e6c2b584e44bd2600d9cde7b0132389df984
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-04-05 19:05:25 +0200

    Zoom in/out with +/- in imageviewer

--- iv.py
+++ iv.py
@@ -6,16 +6,21 @@ import pylibpng
 def show_png(surface, png, zoom=1):
     for h in range(0, png.height):
         for w in range(0, png.width):
-            surface.fill(png.pixels[h][w], ((w*zoom,h*zoom), (zoom,zoom)))
+            try:
+                surface.fill(png.pixels[h][w], ((w*zoom,h*zoom), (zoom,zoom)))
+            except IndexError:
+                print("%i, %i" % (h,w))
 
 if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        zoom = int(sys.argv[2])
+    else:
+        zoom = 1
+    pic = pylibpng.PNG(sys.argv[1])
+    black = (0,0,0)
 
     pygame.init()
-
-    pic = pylibpng.PNG(sys.argv[1])
-    black = (255,255,255)
-
-    screen = pygame.display.set_mode((pic.width,pic.height))
+    screen = pygame.display.set_mode((pic.width*zoom,pic.height*zoom), pygame.RESIZABLE)
     clock = pygame.time.Clock()
     
     if pic.bkgd:
@@ -23,13 +28,24 @@ if __name__ == '__main__':
     else:
         screen.fill(black)
 
-    show_png(screen, pic, 1)
-
+    show_png(screen, pic, zoom)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PLUS:
+                    zoom += 1
+                    show_png(screen, pic, zoom)
+                elif event.key == pygame.K_MINUS:
+                    if zoom > 1:
+                        zoom -= 1
+                        show_png(screen, pic, zoom)
+            elif event.type == pygame.VIDEORESIZE:
+                screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                screen.fill(black)
+                show_png(screen, pic, zoom)
         pygame.display.update()
         clock.tick(30)
 
